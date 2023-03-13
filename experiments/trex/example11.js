54af34c5c23a856ca8b6866736b632510dd93e81
MadhuNimmo <mchak009@ucr.edu>
2023-03-12 20:29:11 -0700

    example11 added

---
+++ experiments/trex/example11.js
@@ -0,0 +1,48 @@
+function foo() {
+        return there(hi)
+}
+
+function bar() {
+        return
+}
+
+function hi() {
+        return hello
+}
+
+function hello() {
+        return
+}
+
+function there(x) {
+        y = x()
+        y()
+        return bar
+}
+
+function buyNow(x) {
+        addToCart(x)
+}
+
+function done() {
+        return
+}
+
+function completeBuy() {
+        console.log("Purchased")
+        return done
+}
+
+function addToCart(y) {
+        y()
+}
+
+var x = foo()
+x()
+x()
+var y = hi()
+y()
+there(foo)
+
+// buyNow(completeBuy)
+// addToCart(completeBuy)
